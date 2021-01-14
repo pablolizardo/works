e45e65f22e42221247c65501c2fb1d68490c4066
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2021-01-14 00:01:14 -0300

    modal

--- pages/illustrations.js
+++ pages/illustrations.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 const fs = require('fs')
 const path = require('path')
 
 function illustration({ images }) {
+    const [showModal, setShowModal] = useState()
+    const handleOpenModal = (filename) => { setShowModal(filename) }
+    const handleCloseModal = () => { setShowModal() }
     return (
         <div>
             <h1>Illustrations</h1>
+            {showModal &&
+            <div id="backdrop" onClick={handleCloseModal}>
+                    <img src={showModal} className='backdrop__image' />
+            </div>
+            }
             { Object.keys(images)
                 .map(folder =>
                     <section key={folder}>
                         <h2>{folder}</h2>
                         <div className='flex flex-wrap illustrations'>
                             {images[folder].map((image, index) =>
-                                <Image className='illustration' key={`${folder}-${index}`} src={`/images/illustrations/${folder}/${image}`} width='64' height='64' objectFit='cover' />
+                                <Image onClick={()=>handleOpenModal(`/images/illustrations/${folder}/${image}`)} className='illustration' key={`${folder}-${index}`} src={`/images/illustrations/${folder}/${image}`} width='64' height='64' objectFit='cover' />
                             )}
                         </div>
                     </section>
