3fe172d0d82e0e792aa13dac4116fe4b806cf798
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2021-01-14 00:58:20 -0300

    feat:

--- pages/illustrations.js
+++ pages/illustrations.js
@@ -11,17 +11,26 @@ function illustration({ images }) {
         <div>
             <h1>Illustrations</h1>
             {showModal &&
-            <div id="backdrop" onClick={handleCloseModal}>
+                <div id="backdrop" onClick={handleCloseModal}>
                     <img src={showModal} className='backdrop__image' />
-            </div>
+                </div>
             }
+            <getSelection>
+                <h2>The Wind</h2>
+                <div className='flex flex-wrap illustrations'>
+                    {images['The Wind'].map((image, index) =>
+                        <Image onClick={() => handleOpenModal(`/images/illustrations/The Wind/${image}`)} className='illustration' key={`The Wind-${index}`} src={`/images/illustrations/The Wind/${image}`} width='128' height='128' objectFit='cover' />
+                    )}
+                </div>
+            </getSelection>
             { Object.keys(images)
+                .filter( folder => folder !== 'The Wind')
                 .map(folder =>
                     <section key={folder}>
                         <h2>{folder}</h2>
                         <div className='flex flex-wrap illustrations'>
                             {images[folder].map((image, index) =>
-                                <Image onClick={()=>handleOpenModal(`/images/illustrations/${folder}/${image}`)} className='illustration' key={`${folder}-${index}`} src={`/images/illustrations/${folder}/${image}`} width='64' height='64' objectFit='cover' />
+                                <Image onClick={() => handleOpenModal(`/images/illustrations/${folder}/${image}`)} className='illustration' key={`${folder}-${index}`} src={`/images/illustrations/${folder}/${image}`} width='64' height='64' objectFit='cover' />
                             )}
                         </div>
                     </section>
