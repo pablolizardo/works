7ddca48a3efcdd69570cf8b0562c1ad542a7f5e0
Pablo Lizardo <pablo.lizardo@blackboard.com>
2022-11-10 13:02:57 +0000

    lichess test

--- components/WorkLink.js
+++ components/WorkLink.js
@@ -6,19 +6,22 @@ import styles from './WorkLink.module.css';
 
 function WorkLink({ project }) {
   return (
-    <Link href={`/projects/${project.fields.slug}`} className={styles.container}>
-      <li className={styles.item}>
-        <div className={styles.left}>
-          <h2 className={styles.title}>{project.fields.title}</h2>
-          <p className={styles.subtitle}>{project.fields.subtitle}</p>
-          {project.fields.hashtags && <BadgesList size="xs" tags={project.fields.hashtags} />}
-        </div>
-        <div className={styles.image}>
-          {project.fields.image && (
-            <Image alt={project.fields.title} src={`https:${project.fields.image.fields.file.url}`} width={300} height={130} layout="responsive" objectFit="cover" objectPosition="top"></Image>
-          )}
-        </div>
-      </li>
+    <Link href={`/projects/${project.fields.slug}`} >
+      <a >
+
+        <li className={styles.item}>
+          <div className={styles.left}>
+            <h2 className={styles.title}>{project.fields.title}</h2>
+            <p className={styles.subtitle}>{project.fields.subtitle}</p>
+            {project.fields.hashtags && <BadgesList size="xs" tags={project.fields.hashtags} />}
+          </div>
+          <div className={styles.image}>
+            {project.fields.image && (
+              <Image alt={project.fields.title} src={`https:${project.fields.image.fields.file.url}`} width={300} height={130} layout="responsive" objectFit="cover" objectPosition="top"></Image>
+            )}
+          </div>
+        </li>
+      </a>
     </Link>
   );
 }
--- pages/me.js
+++ pages/me.js
@@ -4,38 +4,46 @@ const { Converter } = require('showdown');
 const path = require('path');
 import Head from 'next/head'
 
-function me({html}) {
-    return (
-        <div>
-          <Head>
-                <title key='title'>About me 👨‍💻</title>
-            </Head>
-            <h1>About me 👋</h1>
-            <main dangerouslySetInnerHTML={{ __html : html }} />
-        </div>
-    )
+function me({ html, lichess }) {
+  return (
+    <div>
+      <Head>
+        <title key='title'>About me 👨‍💻</title>
+      </Head>
+      <h1>About me 👋</h1>
+      <main dangerouslySetInnerHTML={{ __html: html }} />
+      <dl>
+        <dt>♟ Lichess Blitz Rating</dt>
+        <dd><a href='https://lichess.org/@/pablolizardo' target="_blank">{lichess.perfs.blitz.rating}</a></dd>
+      </dl>
+    </div>
+  )
 }
 
 export default me
 
 
 export async function getStaticProps() {
+  const lichessURL = 'https://lichess.org/api/user/pablolizardo'
+  const lichessAPI = await fetch(lichessURL);
+  const lichesJSON = await lichessAPI.json();
+  console.log(lichesJSON)
 
-    const converter = new Converter();
-  
-    function parseREADME() {
-      return new Promise((res) => {
-        fs.readFile(path.join(process.cwd(), '/content/me.md'), (err, data) => {
-          const readme = data.toString();
-          const html = converter.makeHtml(readme);
-          res(html);
-        });
+
+  const converter = new Converter();
+  function parseREADME() {
+    return new Promise((res) => {
+      fs.readFile(path.join(process.cwd(), '/content/me.md'), (err, data) => {
+        const readme = data.toString();
+        const html = converter.makeHtml(readme);
+        res(html);
       });
-    }
-  
-    const html = await parseREADME();
-  
-    return {
-      props: { html },
-    };
+    });
   }
+
+  const html = await parseREADME();
+
+  return {
+    props: { html, lichess: lichesJSON },
+  };
+}
